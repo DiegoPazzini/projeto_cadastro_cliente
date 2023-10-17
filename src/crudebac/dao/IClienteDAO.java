aa0cb27066f11a906e674572f4f54680bc5297c1
Diego Pazzini <diegopazzini2009@hotmail.com>
2023-10-17 08:09:00 -0300

    Update IClienteDAO.java

--- src/crudebac/dao/IClienteDAO.java
+++ src/crudebac/dao/IClienteDAO.java
@@ -1,23 +1,22 @@
+package crudebac.dao; // Pacote que contém as classes relacionadas à manipulação de dados de clientes.
 
-package crudebac.dao;
-
-import crudebac.domain.Cliente;
-import java.util.Collection;
+import crudebac.domain.Cliente; // Importação da classe Cliente do pacote crudebac.domain.
+import java.util.Collection; // Importação da interface Collection do pacote java.util para uso genérico de coleções.
 
 /**
- *
- * @author Wendell Marcos
+ * Interface que representa as operações básicas de um DAO (Data Access Object) para a entidade Cliente.
+ * O autor desta interface é Diego Pazzini.
  */
 public interface IClienteDAO {
-    
-    public Boolean cadastrar(Cliente cliente);
-    
-    public void excluir(Long cpf);
-    
-    public void alterar(Cliente cliente);
-    
-    public Cliente consultar(Long cpf);
-    
-    public Collection<Cliente> buscarTodos();
-    
+
+    public Boolean cadastrar(Cliente cliente); // Método para cadastrar um cliente no banco de dados.
+
+    public void excluir(Long cpf); // Método para excluir um cliente do banco de dados com base no CPF.
+
+    public void alterar(Cliente cliente); // Método para alterar os detalhes de um cliente existente no banco de dados.
+
+    public Cliente consultar(Long cpf); // Método para consultar e recuperar os detalhes de um cliente com base no CPF.
+
+    public Collection<Cliente> buscarTodos(); // Método para buscar e retornar todos os clientes presentes no banco de dados.
+
 }
